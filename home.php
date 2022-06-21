e459ccaa1df22e695737b6258e7338b576b5ff91
dominic2486 <70656707+dominic2486@users.noreply.github.com>
2022-06-20 21:39:23 -0400

    fixing home.php

--- home.php
+++ home.php
@@ -44,6 +44,9 @@
 
     <?php
 require_once(__DIR__ . "/lib/helpers.php");
+if(!is_logged_in()){
+    header('Location:index.php');
+}
 echo '<p>Welcome ' . get_user_fullname() . '</p>';
 ?>
 <form action="logout.php">

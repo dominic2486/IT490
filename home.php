05cecabda8a0d8eef91874ebedb42524c7ecbae5
dominic2486 <70656707+dominic2486@users.noreply.github.com>
2022-06-20 21:55:22 -0400

    adding notifcations

--- home.php
+++ home.php
@@ -44,10 +44,9 @@
 
     <?php
 require_once(__DIR__ . "/lib/helpers.php");
+require_once(__DIR__ . "flash.php"); 
 if(!is_logged_in()){
-    session_unset();
-    session_destroy();
-    echo '<p> user must sign in first.</p>';
+    flash('<p> user must sign in first.</p>');
     header('Location:index.php');
 }
 echo '<p>Welcome ' . get_user_fullname() . '</p>';
